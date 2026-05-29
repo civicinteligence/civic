11486b61e8032de7271fbe59fe5477ef6b5916fb
kimtech <ugkimtech@gmail.com>
2026-05-30 00:55:28 +0300

    added visualization dashbord

--- civic/dashboards/views.py
+++ civic/dashboards/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
-
+from intelligence.models import Results
+from django.db.models import F
+from django.core.serializers.json import DjangoJSONEncoder
+import json
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+
+    reports_qs = Results.objects.all()
+    reports = list(reports_qs.values(
+        "id",
+        "category",
+        "urgency",
+        "location",
+        "status",
+        message= F("report__message"),
+        time=F("report__time"),
+        area=F("report__location"),
+    ))
+
+    data = {
+        "reports": json.dumps(reports, cls=DjangoJSONEncoder),
+        "total": reports_qs.count(),
+        "high_urgency": Results.objects.filter(urgency='high').count(),
+        "low_urgency": Results.objects.filter(urgency='low').count(),
+        "resolved_issues": Results.objects.filter(status='resolved').count(),
+        "water_issues": Results.objects.filter(category='water').count(),
+        "roads_issues": Results.objects.filter(category='roads').count(),
+        "health_issues": Results.objects.filter(category='health').count(),
+        "garbage_issues": Results.objects.filter(category='garbage').count(),
+    }
+
+    return render(request, 'dashboard.html', {"data": data})
--- civic/intelligence/models.py
+++ civic/intelligence/models.py
@@ -8,3 +8,4 @@ class Results(models.Model):
     category = models.CharField(max_length=50)
     urgency = models.CharField(max_length=20)
     location = models.CharField(max_length=50) #if from NLP
+    status = models.CharField(max_length=10, default='pending')
